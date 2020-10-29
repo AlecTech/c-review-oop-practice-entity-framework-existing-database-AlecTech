c44849ee0455cfcd034b55b3ab2e29c19b3bc513
AlecTech <eremeevoleg@gmail.com>
2020-10-29 16:38:25 -0600

    Added query to print out full name from DB;

--- ExistingDBPractice/Program.cs
+++ ExistingDBPractice/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ExistingDBPractice.Model;
+using System;
+using System.Linq;
 
 namespace ExistingDBPractice
 {
@@ -6,7 +8,18 @@ namespace ExistingDBPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input;
+            Console.Write("Please Enter First Name to lookup in DB:");
+            input = Console.ReadLine().Trim();
+
+            using(PersonContext context = new PersonContext())
+            {
+                Person person = context.Person.Where(x => x.FirstName.ToLower() == input.ToLower()).Single();
+
+                //List<Vehicle> vehicles = context.Vehicles.Where(x => x.ManufacturerId == manufacturer.Id).ToList();
+
+                Console.WriteLine($"-{person.FirstName} {person.LastName}");
+            }
         }
     }
 }
