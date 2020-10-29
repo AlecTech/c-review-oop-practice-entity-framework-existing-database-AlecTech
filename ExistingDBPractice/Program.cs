92b8118345285e363c24c99dfe2fd8b724392682
AlecTech <eremeevoleg@gmail.com>
2020-10-29 17:57:10 -0600

    added comments;

--- ExistingDBPractice/Program.cs
+++ ExistingDBPractice/Program.cs
@@ -21,13 +21,14 @@ namespace ExistingDBPractice
 
                     try
                     {
-                        
+                        //get everything from context.Person first, then chose only that row which is equals to the input from the user
                         Person person = context.Person.Where(x => x.FirstName.ToLower() == input.ToLower()).Single();
-
+                        //JOIN Person table with Phonenumber table based on PersonId and ID columns
                         List<Phonenumber> phone = context.Phonenumber.Where(x => x.PersonId == person.Id).ToList();
 
+                        // if "phone" object has 3 #s assosiated with input name it will loop 3 times.
                         foreach (Phonenumber phones in phone)
-                        {
+                        {   //print out row from person query FirstName and LastName and then look into phone object and grab all phones that correlate with PersonId 
                             Console.WriteLine($"-{person.FirstName} {person.LastName} {phones.Number}");
                         }
                         
@@ -35,11 +36,10 @@ namespace ExistingDBPractice
 
                     catch
                     {
-                        Console.WriteLine("ERROR: Name not found.");
-                        //input = "";
+                        Console.WriteLine("ERROR: Name not found.");     
                     }
 
-                }
+                }//if true keep looping
             } while (input != "quit");
         }
     }
